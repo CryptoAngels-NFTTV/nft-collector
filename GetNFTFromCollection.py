5e9b2dd08ac7acee0b169b32d2fe0d302f978739
Rafael Chiarinelli <rchiarinelli@gmail.com>
2023-02-20 17:41:16 +0000

    Added Exception handling

--- GetNFTFromCollection.py
+++ GetNFTFromCollection.py
@@ -5,14 +5,22 @@ import os
 
 api_key = "KE9G7lVZwm7soA1SuaQc61DnUAyrFxNI56Q5e2SsiXaX2UKvYVGXcaNHrJrqNful"
 
+# 'eth' , 'goerli' , 'sepolia' , 'polygon' , 'mumbai' , 'bsc' , 'bsc testnet' , 'avalanche' , 'avalanche testnet' , 'fantom' , 'palm' , 'cronos' , 'cronos testnet' , 'arbitrum'
+
 # '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB' - crypto punks - eth
 # '0xea1635a0e9344d933df42c0fd494d39bce865dc4' - loaded lions - cronos
 # '0x314a2F0311D184d4C5529da578390F5bED45f865'- pyscho kitties - eth
 # '0x50332bdca94673F33401776365b66CC4e81aC81d'- cryptocards - bsc
 # '0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949'- beanz - eth
 
-contractAddresses = ['0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949', '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB',
-                     '0xea1635a0e9344d933df42c0fd494d39bce865dc4', '0x314a2F0311D184d4C5529da578390F5bED45f865', '0x50332bdca94673F33401776365b66CC4e81aC81d']
+
+contractAddresses = {
+    'bsc':['0x50332bdca94673F33401776365b66CC4e81aC81d'],
+    'cronos': ['0xea1635a0e9344d933df42c0fd494d39bce865dc4'],
+    'eth': ['0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB','0x314a2F0311D184d4C5529da578390F5bED45f865','0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949']
+}
+
+
 
 params = {
     "address": "",
@@ -23,13 +31,6 @@ params = {
     "cursor": "",
     "normalizeMetadata": True,
 }
-
-# params = {
-#     "address": "",
-#     "chain": "eth",
-#     "limit": 100,
-#     "cursor": "",
-# }
 
 amqpHost = "amqp://localhost?connection_attempts=5&retry_delay=5"
 
@@ -54,20 +55,20 @@ channel.queue_bind(exchange='nfts-exchange',
 
 results = []
 
-# 'eth' , 'goerli' , 'sepolia' , 'polygon' , 'mumbai' , 'bsc' , 'bsc testnet' , 'avalanche' , 'avalanche testnet' , 'fantom' , 'palm' , 'cronos' , 'cronos testnet' , 'arbitrum'
-
-chains = ['eth', 'cronos', 'bsc']
-
-for address in contractAddresses:
-    for chain in chains:
-        params['address'] = address
+try:
+    for chain in contractAddresses:
         params['chain'] = chain
-        print("Params: ", json.dumps(params, sort_keys=True, indent=4))
-        results += [evm_api.nft.get_wallet_nfts(
-            api_key=api_key,
-            params=params,
-        )]
-        print("Partial: ", json.dumps(results, sort_keys=True, indent=4))
+        for address in contractAddresses[chain]:
+            params['address'] = address
+            print("Params: ", json.dumps(params, sort_keys=True, indent=4))
+            results += [evm_api.nft.get_wallet_nfts(
+                api_key=api_key,
+                params=params,
+            )]
+            print("Partial: ", json.dumps(results, sort_keys=True, indent=4))
+except Exception as e:
+    print("Exception collecting data from Moralis API:", json.dumps(e, sort_keys=True, indent=4))
+    
 
 for nftMetada in results:
     for res in nftMetada["result"]:
